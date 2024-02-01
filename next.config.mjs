799e42c66a5ec299df79307a3ecc0a4dec2d7779
ganningorlightman <ganningorlightman@gmail.com>
2024-02-01 14:24:45 +0300

    path fix

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
   output: "export",
   images: {
     loader: 'akamai',
-    path: '',
-    domains: ['https://ganningorlightman.github.io/deploying-next'],
+    path: isProd ? 'deploying-next' : 'out',
+    domains: ['https://ganningorlightman.github.io'],
   },
   assetPrefix: isProd ? 'https://ganningorlightman.github.io/deploying-next' : '',
 };
