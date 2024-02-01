0e8855ad87bdb80cd30b54e55576563a2ced52e8
ganningorlightman <ganningorlightman@gmail.com>
2024-02-01 14:01:45 +0300

    css fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /**
 * @type {import('next').NextConfig}
 */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: "export",
   images: {
     loader: 'akamai',
     path: '',
   },
+  assetPrefix: isProd ? 'https://ganningorlightman.github.io/deploying-next' : undefined,
 };
 
 export default nextConfig;
